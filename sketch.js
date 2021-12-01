9a8d7b26004d4e09e293a665efd81036597c9adb
Vitin <victorbrph9@gmail.com>
2021-12-01 14:39:05 -0300

    add new ground

--- sketch.js
+++ sketch.js
@@ -5,17 +5,20 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg;
+var ground2;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
+
 }
 
 function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
+   
 
-    
+    ground2 = new Ground(150,300,300,200);
     ground = new Ground(600,height,1200,20)
 
     box1 = new Box(700,320,70,70);
@@ -48,6 +51,7 @@ function draw(){
     ground.display();
     pig1.display();
     log1.display();
+    ground.display();
 
     box3.display();
     box4.display();
